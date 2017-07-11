7bed307ea529cce99a114818f1b3589f12cc6752
我爱Github <taoste@gmail.com>
2017-07-11 19:05:58 +0800

    add VIP-Video

---
+++ index.php
@@ -0,0 +1,314 @@
+﻿
+<!DOCTYPE html>
+<html lang="zh-cn">
+<head>
+  <meta charset="utf-8"/>
+  <meta name="viewport" content="width=100%, initial-scale=1"/>
+  <title>乐哥 VIP视频解析站-为亲爱的张龙梅子而做的视频网站</title>
+  <link rel="shortcut icon" href="images/favicon.png">
+  <meta name="keywords" content="vip视频解析,视频解析,乐哥视频,乐哥vip视频解析站" />
+  <meta name="description" content="小人物做的小网站，只是为了我的朋友们，希望访问本网站的人都是我的朋友" />
+  <link href="//cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet"/>
+  <script src="//cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script>
+  <script src="//cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  <script src="css/jquery-1.7.2.min.js"></script>
+  <script type="text/javascript">eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('b a(){0 6=1.2("9").4;0 5=1.2("3");0 3=1.2("3").c;0 8=5.e[3].4;0 7=1.2("f");7.d=8+6}',16,16,'var|document|getElementById|jk|value|jkurl|diz|cljurl|jkv|url|dihejk|function|selectedIndex|src|options|player'.split('|'),0,{}))</script>
+  <script type="text/javascript">var OriginTitile = document.title;
+      var titleTime;
+      document.addEventListener('visibilitychange',
+      function() {
+        if (document.hidden) {
+          document.title = 'φ(>ω<*) 这有好东西哦~ ' + OriginTitile;
+          clearTimeout(titleTime);
+        } else {
+          document.title = '( • ̀ω•́ )✧恭喜你发现了~ ' + OriginTitile;
+          titleTime = setTimeout(function() {
+            document.title = OriginTitile;
+          },
+          2000);
+        }
+      });</script>
+	  <script type="text/javascript">
+</script>
+<style type="text/css">
+jjgg {display: inline}
+ggkj {display: none}
+</style>
+
+  </head>
+<body style="background: url(&quot;images/bj.jpg&quot;);">
+
+</nav> 
+
+  <div class="col-xs-12 col-sm-10 col-md-8 col-lg-8 center-block" style="float: none;">
+<h2 class="text-muted" style="color:#F5FFFA" align="center"><strong href="/"  title="乐哥-为亲爱的张龙梅子而做的视频网站">乐哥 VIP会员视频解析站</strong></h2>
+  <center> 
+   <span style="color:E20000"><marquee behavior="scroll"><strong>
+    乐哥vip会员视频解析站上线啦！……这个项目仅仅是为我的爱人<B>张龙梅子</B>而弄的，朋友们喜欢就好。（视频接口失效请到乐哥个人网站留言告知，我有时间会更新！）   </strong>
+   </marquee>
+   </span>
+  </center>
+  
+<form action="http://kan.sogou.com/search" target="_blank">
+ <div align="center"> 
+  <table bgcolor="#87CEEB"> 
+   <tr>
+    <td> 
+     <div class="input-group" style="width: 100%;">
+      <input class="form-control input-lg" type="text" placeholder="全网视频搜索"name=keyword size=30">  
+	  <span class="input-group-addon input-lg" style="width: 80px;">
+	  <button id="bf" type="submit" value="">搜索</button>
+	  </span>
+     </div>
+    </td>
+   </tr> 
+  </table> 
+ </div> 
+</form>
+<hr>
+ <table class="table table-bordered">
+  <tbody>
+   <div class="col-md-14 column">
+    <div class="panel panel-default">
+    <div id="kj" class="panel-body">
+    <iframe src="" id="player" width="100%" height="450px" allowtransparency="true" frameborder="0" scrolling="no"></iframe>
+    </div>
+    </div>
+   </div>
+    <td>
+    <table bgcolor="#87CEEB">
+    <div class="col-md-14 column">
+    <form method="get" >
+	<div class="col-md-5 column" style="margin-top: 2px;">
+    <div class="input-group" style="width: 100%;">
+	<span class="input-group-addon input-lg" style="width: 80px; ">接口</span>
+    <select class="form-control input-lg" id="jk">
+	<option value="http://api.47ks.com/webcloud/?v="  style="color:red">1号通用接口👍</option>
+	<option value="http://vip.72du.com/api/?url=" style="color:red">2号通用接口👍</option>
+	<option value="http://qtzr.net/s/?qt=">3号通用接口</option>
+	<option value="http://000o.cc/jx/ty.php?url=">4号通用接口</option>
+	<option value="http://yyygwz.com/index.php?url=">5号通用接口</option>
+	<option value="http://www.wmxz.wang/video.php?url=">6号通用接口</option> 
+	<option value="http://yun.zihu.tv/play.html?url=">7号通用接口</option>
+	<option value="http://www.vipjiexi.com/yun.php?url=">8号通用接口</option>
+	<option value="https://jxapi.nepian.com/ckparse/?url=">9号通用接口</option>
+	<option value="http://2gty.com/apiurl/yun.php?url=">10号通用接口</option>
+	 <option value="http://www.chepeijian.cn/jiexi/vip.php?url=">11号通用接口</option>	
+	 <option value="https://apiv.ga/magnet/">万能磁力链接解析</option>
+	<option value="http:// ">更多接口待添加...</option></select>
+    </div>
+	</div>
+    <div class="col-md-5" style="margin-top: 2px;">
+    <div class="input-group" style="width: 100%;">
+    <input class="form-control input-lg" type="search" placeholder="输入需要会员的视频播放地址" id="url" autocomplete="off">
+    </div></div>
+    <div class="col-md-2" style="margin-top: 2px;">
+    <button id="bf" type="button" class="btn btn-success btn-lg btn-block" onClick="dihejk()">播放</button>
+    </div>
+    </div>
+    </form>
+    </div>
+	</table> 
+   </td> 
+    </tbody>
+</table>
+    <table class="table table-bordered">
+	<thead>
+  <ggkj><div class="alert alert-success alert-dismissible" role="alert">
+  <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span></button>
+  <strong>
+  <b title="本站的广告只放有用的">广告：</b>
+	<a target="_blank" href="#" class="btn btn-default btn-sm">没广告</a>  </strong></div></ggkj></thead>
+  <jjgg><div class="alert alert-success alert-dismissible" role="alert">
+  <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span></button>
+  <strong>
+  <b title="本站的公告">公告： </b>
+  乐哥vip视频解析站上线！这个项目仅仅是为我的爱人张龙梅子而弄的，朋友们喜欢就好。<br>由于我本人一般都是下载电影、电视剧回来看的，很少在这看，所以接口可能不能实时更新。  </strong></div></jjgg>
+<h3 class="text-muted" style="color:#F5FFFA" align="center"><strong> 接口可用情况（如失效请联系我） </strong></h3>
+<h5 class="text-muted" style="color:#F5FFFA" align="center"><strong> 接口最后验证时间2017年07月07日。本资料仅供参考，最好请自行测试 </strong></h5>
+<style type="text/css">
+* {
+	padding:0;
+	margin:0;
+}
+.wrap {
+	width:60%;
+	margin:1% auto 0;
+}
+table {
+	border-collapse:collapse;
+	border-spacing:0;
+	border:1px solid #c0c0c0;
+	width:100%;
+}
+th,td {
+	border:1px solid #d0d0d0;
+	color:#404040;
+	padding:1%;
+}
+th {
+	background-color:#09c;
+	font:bold 16px "微软雅黑";
+	color:#fff;
+}
+td {
+	font:14px "微软雅黑";
+	color:#9932CC;
+}
+tbody tr {
+	background-color:#f0f0f0;
+}
+tbody tr:hover {
+	cursor:pointer;
+	background-color:#fafafa;
+}
+</style>
+<div class="wrap">
+    <table>
+        <thead>
+            <tr>
+               
+                <th>名称</th>
+				<th>优酷</th>
+                <th>爱奇艺</th>
+				<th>腾讯</th>
+				<th>乐视</th>
+				<th>搜狐</th>
+				<th>芒果</th>
+				<th>PPTV</th>
+				<th>华数</th>
+            </tr>
+        </thead>
+       
+            <tr>
+                <td>1号接口</td>
+				<td style="color:#5CB85C" >✔</td>
+				<td style="color:#BF0077" >✖</td>
+                <td style="color:#BF0077" >✖</td>
+				<td style="color:#5CB85C" >✔</td>
+				<td style="color:#5CB85C" >✔</td>
+				<td style="color:#5CB85C" >✔</td>
+				<td style="color:#5CB85C" >✔</td>
+				<td style="color:#5CB85C" >✔</td>
+            </tr>
+            <tr>
+               <td>2号接口</td>
+				<td style="color:#5CB85C" >✔</td>
+				<td style="color:#5CB85C" >✔</td>
+                <td style="color:#BF0077" >✖</td>
+				<td style="color:#5CB85C" >✔</td>
+				<td style="color:#5CB85C" >✔</td>
+				<td style="color:#5CB85C" >✔</td>
+				<td style="color:#5CB85C" >✔</td>
+				<td style="color:#5CB85C" >✔</td>
+            </tr>
+            <tr>
+                <td>3号接口</td>
+				<td style="color:#5CB85C" >✔</td>
+				<td style="color:#BF0077" >✖</td>
+                <td style="color:#BF0077" >✖</td>
+				<td style="color:#5CB85C" >✔</td>
+				<td style="color:#5CB85C" >✔</td>
+				<td style="color:#BF0077" >✖</td>
+				<td style="color:#5CB85C" >✔</td>
+				<td style="color:#5CB85C" >✔</td>
+            </tr>
+            <tr>
+                <td>4号接口</td>
+				<td style="color:#5CB85C" >✔</td>
+				<td style="color:#BF0077" >✖</td>
+                <td style="color:#BF0077" >✖</td>
+				<td style="color:#5CB85C" >✔</td>
+				<td style="color:#5CB85C" >✔</td>
+				<td style="color:#5CB85C" >✔</td>
+				<td style="color:#BF0077" >✖</td>
+				<td style="color:#5CB85C" >✔</td>
+            </tr>
+			<tr>
+                <td>5号接口</td>
+				<td style="color:#BF0077" >✖</td>
+				<td style="color:#BF0077" >✖</td>
+                <td style="color:#BF0077" >✖</td>
+				<td style="color:#5CB85C" >✔</td>
+				<td style="color:#5CB85C" >✔</td>
+				<td style="color:#5CB85C" >✔</td>
+				<td style="color:#BF0077" >✖</td>
+				<td style="color:#5CB85C" >✔</td>
+            </tr>
+			<tr>
+                <td>6号接口</td>
+				<td style="color:#5CB85C" >✔</td>
+				<td style="color:#5CB85C" >✔</td>
+                <td style="color:#BF0077" >✖</td>
+				<td style="color:#5CB85C" >✔</td>
+				<td style="color:#5CB85C" >✔</td>
+				<td style="color:#5CB85C" >✔</td>
+				<td style="color:#BF0077" >✖</td>
+				<td style="color:#5CB85C" >✔</td>
+            </tr>
+			<tr>
+                <td>7号接口</td>
+				<td style="color:#5CB85C" >✔</td>
+				<td style="color:#BF0077" >✖</td>
+                <td style="color:#5CB85C" >✔</td>
+				<td style="color:#BF0077" >✖</td>
+				<td style="color:#5CB85C" >✔</td>
+				<td style="color:#5CB85C" >✔</td>
+				<td style="color:#BF0077" >✖</td>
+				<td style="color:#5CB85C" >✔</td>
+            </tr>
+			<tr>
+                <td>8号接口</td>
+				<td style="color:#5CB85C" >✔</td>
+				<td style="color:#BF0077" >✖</td>
+                <td style="color:#BF0077" >✖</td>
+				<td style="color:#5CB85C" >✔</td>
+				<td style="color:#5CB85C" >✔</td>
+				<td style="color:#5CB85C" >✔</td>
+				<td style="color:#BF0077" >✖</td>
+				<td style="color:#5CB85C" >✔</td>
+            </tr>
+			<tr>
+                <td>9号接口</td>
+				<td style="color:#BF0077" >✖</td>
+				<td style="color:#BF0077" >✖</td>
+                <td style="color:#BF0077" >✖</td>
+				<td style="color:#5CB85C" >✔</td>
+				<td style="color:#BF0077" >✖</td>
+				<td style="color:#5CB85C" >✔</td>
+				<td style="color:#BF0077" >✖</td>
+				<td style="color:#BF0077" >✖</td>
+            </tr>
+			<tr>
+                <td>10号接口</td>
+				<td style="color:#BF0077" >✖</td>
+				<td style="color:#BF0077" >✖</td>
+                <td style="color:#5CB85C" >✔</td>
+				<td style="color:#5CB85C" >✔</td>
+				<td style="color:#5CB85C" >✔</td>
+				<td style="color:#5CB85C" >✔</td>
+				<td style="color:#5CB85C" >✔</td>
+				<td style="color:#5CB85C" >✔</td>
+            </tr>
+			<tr>
+                <td>11号接口</td>
+				<td style="color:#5CB85C" >✔</td>
+				<td style="color:#5CB85C" >✔</td>
+                <td style="color:#BF0077" >✖</td>
+				<td style="color:#5CB85C" >✔</td>
+				<td style="color:#5CB85C" >✔</td>
+				<td style="color:#5CB85C" >✔</td>
+				<td style="color:#5CB85C" >✔</td>
+				<td style="color:#5CB85C" >✔</td>
+            </tr>
+    </table>
+</div>
+
+<div class="panel-body" style="text-align: center;">
+ <div class="container-fluid">
+   <p><br>
+     <span class="copyright">Copyright &copy; 2017 乐哥VIP视频解析站  powered <a href="/" title="乐哥的个人网站" target="_blank">@乐哥在线</a></span>   </p>
+</div>
+</body>
+</html>
+
